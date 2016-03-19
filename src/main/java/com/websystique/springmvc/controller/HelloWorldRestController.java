ed48f8824b3522273197fd57906a8acdad9567d5
Dorothy Rifai <drifai@Dorothys-MBP.fios-router.home>
2016-03-19 15:49:11 -0400

    ping working

--- src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -2,8 +2,10 @@ package com.websystique.springmvc.controller;
  
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
- 
+
+import com.bestride.platform.log.LoggingUtils;
 import com.websystique.springmvc.domain.Message;
  
 @RestController
@@ -15,5 +17,11 @@ public class HelloWorldRestController {
         Message msg = new Message(player, "Hello " + player);
         return msg;
     }
+    
+	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	public String ping() {
+		LoggingUtils.logInfo("Validation Controller pinged successfully...");
+		return "Ping Successful";
+	}
  
 }
